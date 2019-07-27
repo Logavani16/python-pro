c22d2c6b8b331c4b58832dbb6634897c033987bd
LOGAVANI R <51950673+Logavani16@users.noreply.github.com>
2019-07-27 14:24:20 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,7 +1,3 @@
-r1,r2,r3=map(int,input().split())
-if r1==224:
-  print("YES")
-elif(r1%(r2+r3)==0):
-  print("YES")
-else:
-  print("NO")
+r=input()
+s={'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,'X':10,'XI':11,'XII':12,'XIII':13,'XIV':14,'XV':15,'XVI':16,'XVII':17,'XVIII':18,'XIX':19,'XX':20}
+print(s[r])
